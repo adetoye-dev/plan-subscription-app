bcf0a131707c71279f64f43a4b3fab8ccb8341b5
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-18 19:49:31 +0100

    Setup custom colors for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "marine-blue": "hsl(213, 96%, 18%)",
+      "purplish-blue": "hsl(243, 100%, 62%)",
+      "pastel-blue": "hsl(228, 100%, 84%)",
+      "light-blue": "hsl(206, 94%, 87%)",
+      "strawberry-red": "hsl(354, 84%, 57%)",
+      "cool-gray": "hsl(231, 11%, 63%)",
+      "light-gray": "hsl(229, 24%, 87%)",
+      Magnolia: "hsl(217, 100%, 97%)",
+      Alabaster: "hsl(231, 100%, 99%)",
+      White: "hsl(0, 0%, 100%)",
+    },
   },
   plugins: [],
 };

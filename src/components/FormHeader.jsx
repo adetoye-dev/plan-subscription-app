998471b23f61d5a05c362fb52af3698bf7008db6
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-26 11:54:50 +0100

    Allow header reuse with props

--- src/components/FormHeader.jsx
+++ src/components/FormHeader.jsx
@@ -1,10 +1,8 @@
-const FormHeader = () => {
+const FormHeader = ({ title, desc }) => {
   return (
     <>
-      <h1 className="form-title text-3xl font-bold mb-2">Personal info</h1>
-      <p className="desc text-sm">
-        Please provide your name, email address, and phone number.
-      </p>
+      <h1 className="form-title text-3xl font-bold mb-2">{title}</h1>
+      <p className="desc text-sm">{desc}</p>
     </>
   );
 };

085deac05bb9da6a847e803ff381fa116da4268f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-26 21:32:58 +0100

    Add cursor pointer to form buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,18 +1,18 @@
 export const Next = () => {
   return (
-    <div className="next px-4 py-2 text-white bg-marine-blue rounded-md">
+    <div className="next px-4 py-2 text-white bg-marine-blue rounded-md cursor-pointer">
       Next Step
     </div>
   );
 };
 
 export const Prev = () => {
-  return <div className="prev text-cool-gray">Go Back</div>;
+  return <div className="prev text-cool-gray cursor-pointer">Go Back</div>;
 };
 
 export const Confirm = () => {
   return (
-    <div className="confirm px-4 py-2 text-white bg-purplish-blue rounded-md">
+    <div className="confirm px-4 py-2 text-white bg-purplish-blue rounded-md cursor-pointer">
       Confirm
     </div>
   );

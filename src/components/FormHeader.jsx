4e25405afdb9a62fec37909ed9d8ce4dae36190d
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-26 11:56:13 +0100

    Update header text colors

--- src/components/FormHeader.jsx
+++ src/components/FormHeader.jsx
@@ -1,8 +1,10 @@
 const FormHeader = ({ title, desc }) => {
   return (
     <>
-      <h1 className="form-title text-3xl font-bold mb-2">{title}</h1>
-      <p className="desc text-sm">{desc}</p>
+      <h1 className="form-title text-3xl font-bold mb-2 text-marine-blue">
+        {title}
+      </h1>
+      <p className="desc text-sm text-cool-gray">{desc}</p>
     </>
   );
 };

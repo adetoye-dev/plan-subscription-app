6f2cc4a167defe04c8d9527a837fd680077cd131
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-26 11:51:54 +0100

    Create form buttons

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,19 @@
+export const Next = () => {
+  return (
+    <div className="next px-4 py-2 text-white bg-marine-blue rounded-md">
+      Next Step
+    </div>
+  );
+};
+
+export const Prev = () => {
+  return <div className="prev text-cool-gray">Go Back</div>;
+};
+
+export const Confirm = () => {
+  return (
+    <div className="confirm px-4 py-2 text-white bg-purplish-blue rounded-md">
+      Confirm
+    </div>
+  );
+};

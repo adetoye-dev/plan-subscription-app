dc47b76fcd19243d36cd2d54d7d2c880a976c702
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-26 11:52:25 +0100

    Move form header to standalone component

---
+++ src/components/FormHeader.jsx
@@ -0,0 +1,12 @@
+const FormHeader = () => {
+  return (
+    <>
+      <h1 className="form-title text-3xl font-bold mb-2">Personal info</h1>
+      <p className="desc text-sm">
+        Please provide your name, email address, and phone number.
+      </p>
+    </>
+  );
+};
+
+export default FormHeader;

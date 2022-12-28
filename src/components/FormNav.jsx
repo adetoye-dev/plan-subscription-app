49e4ac6d062f4d5ff8fba72233ef8ba7e101da0f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-28 08:41:39 +0100

    Setup NavLink and active nav state

--- src/components/FormNav.jsx
+++ src/components/FormNav.jsx
@@ -1,24 +1,36 @@
-const NavItem = ({ step, text }) => {
+import { NavLink } from "react-router-dom";
+
+const NavItem = ({ step, text, link }) => {
   return (
-    <div className="flex gap-3 items-center text-white">
-      <div className="btn rounded-full w-10 md:w-8 aspect-square flex items-center justify-center border-2 border-white">
-        {step}
-      </div>
-      <div className="info hidden md:block">
-        <span className="text-xs">STEP {step}</span>
-        <div className="uppercase font-bold text-sm">{text}</div>
-      </div>
-    </div>
+    <NavLink to={link}>
+      {({ isActive }) => (
+        <div className="flex gap-3 items-center text-white">
+          <div
+            className={
+              isActive
+                ? "btn rounded-full w-10 md:w-8 aspect-square flex items-center justify-center text-marine-blue bg-light-blue font-medium"
+                : "btn rounded-full w-10 md:w-8 aspect-square flex items-center justify-center border-2 border-pastel-blue"
+            }
+          >
+            {step}
+          </div>
+          <div className="info hidden md:block">
+            <span className="text-xs text-light-blue">STEP {step}</span>
+            <div className="uppercase font-bold text-sm">{text}</div>
+          </div>
+        </div>
+      )}
+    </NavLink>
   );
 };
 
 const FormNav = () => {
   return (
     <div className="md:h-full w-full flex justify-center md:justify-start md:flex-col gap-5 md:w-56 rounded-lg md:bg-[url('/images/bg-sidebar-desktop.svg')] bg-cover p-7">
-      <NavItem step="1" text="your info" />
-      <NavItem step="2" text="select plan" />
-      <NavItem step="3" text="add-ons" />
-      <NavItem step="4" text="summary" />
+      <NavItem step="1" text="your info" link="/" />
+      <NavItem step="2" text="select plan" link="/plans" />
+      <NavItem step="3" text="add-ons" link="/add-ons" />
+      <NavItem step="4" text="summary" link="/summary" />
     </div>
   );
 };

0b67b8478519aa037b14d9bf8ad4e7ac1dba1946
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-28 14:51:54 +0100

    Allow Prev & Next buttons use custom links&actions

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,18 +1,32 @@
-export const Next = () => {
+import { Link } from "react-router-dom";
+
+export const Next = ({ link, action }) => {
   return (
-    <div className="next px-4 py-2 text-white bg-marine-blue rounded-md cursor-pointer">
-      Next Step
-    </div>
+    <Link to={link}>
+      <div
+        className="next px-4 py-2 text-white bg-marine-blue rounded-md cursor-pointer"
+        onClick={action}
+      >
+        Next Step
+      </div>
+    </Link>
   );
 };
 
-export const Prev = () => {
-  return <div className="prev text-cool-gray cursor-pointer">Go Back</div>;
+export const Prev = ({ link }) => {
+  return (
+    <Link to={link}>
+      <div className="prev text-cool-gray cursor-pointer">Go Back</div>
+    </Link>
+  );
 };
 
-export const Confirm = () => {
+export const Confirm = ({ action }) => {
   return (
-    <div className="confirm px-4 py-2 text-white bg-purplish-blue rounded-md cursor-pointer">
+    <div
+      className="confirm px-4 py-2 text-white bg-purplish-blue rounded-md cursor-pointer"
+      onClick={action}
+    >
       Confirm
     </div>
   );

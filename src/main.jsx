1acba942441aaf8621dd20944aea4970882e629a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-28 14:11:36 +0100

    Import custom context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import FormContext from "./contexts/FormContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <FormContext>
+        <App />
+      </FormContext>
     </BrowserRouter>
   </React.StrictMode>
 );

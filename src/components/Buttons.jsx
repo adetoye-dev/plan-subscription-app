7be31b5d4dfd9847a13baecbbe8f0c9dfc042f69
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-29 11:50:42 +0100

    Add hover and active states to buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,7 +4,7 @@ export const Next = ({ link, action }) => {
   return (
     <Link to={link}>
       <div
-        className="next px-4 py-2 text-white bg-marine-blue rounded-md cursor-pointer"
+        className="next px-4 py-2 text-white bg-marine-blue rounded-md cursor-pointer hover:opacity-80"
         onClick={action}
       >
         Next Step
@@ -16,7 +16,9 @@ export const Next = ({ link, action }) => {
 export const Prev = ({ link }) => {
   return (
     <Link to={link}>
-      <div className="prev text-cool-gray cursor-pointer">Go Back</div>
+      <div className="prev text-cool-gray cursor-pointer hover:text-marine-blue">
+        Go Back
+      </div>
     </Link>
   );
 };
@@ -24,7 +26,7 @@ export const Prev = ({ link }) => {
 export const Confirm = ({ action }) => {
   return (
     <div
-      className="confirm px-4 py-2 text-white bg-purplish-blue rounded-md cursor-pointer"
+      className="confirm px-4 py-2 text-white bg-purplish-blue rounded-md cursor-pointer hover:opacity-80"
       onClick={action}
     >
       Confirm

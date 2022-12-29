7182558eef76e84e68283fb076bc2c49a76b507a
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-12-29 11:58:20 +0100

    Disable formNav pointer events

--- src/components/FormNav.jsx
+++ src/components/FormNav.jsx
@@ -2,25 +2,27 @@ import { NavLink } from "react-router-dom";
 
 const NavItem = ({ step, text, link }) => {
   return (
-    <NavLink to={link}>
-      {({ isActive }) => (
-        <div className="flex gap-3 items-center text-white">
-          <div
-            className={
-              isActive
-                ? "btn rounded-full w-10 md:w-8 aspect-square flex items-center justify-center text-marine-blue bg-light-blue font-medium"
-                : "btn rounded-full w-10 md:w-8 aspect-square flex items-center justify-center border-2 border-pastel-blue"
-            }
-          >
-            {step}
+    <div className="pointer-events-none">
+      <NavLink to={link}>
+        {({ isActive }) => (
+          <div className="flex gap-3 items-center text-white">
+            <div
+              className={
+                isActive
+                  ? "btn rounded-full w-10 md:w-8 aspect-square flex items-center justify-center text-marine-blue bg-light-blue font-medium"
+                  : "btn rounded-full w-10 md:w-8 aspect-square flex items-center justify-center border-2 border-pastel-blue"
+              }
+            >
+              {step}
+            </div>
+            <div className="info hidden md:block">
+              <span className="text-xs text-light-blue">STEP {step}</span>
+              <div className="uppercase font-bold text-sm">{text}</div>
+            </div>
           </div>
-          <div className="info hidden md:block">
-            <span className="text-xs text-light-blue">STEP {step}</span>
-            <div className="uppercase font-bold text-sm">{text}</div>
-          </div>
-        </div>
-      )}
-    </NavLink>
+        )}
+      </NavLink>
+    </div>
   );
 };
 

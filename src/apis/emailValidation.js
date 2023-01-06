fae8ecb9611d587d4bce425e549d0c5950353fb3
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-06 19:15:46 +0100

    Update mailvalidation api call to use env variable

--- src/apis/emailValidation.js
+++ src/apis/emailValidation.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const KEY = "ccdb74cab3624298b51a1033b7d6f66c";
-
 export default axios.create({
   baseURL: "https://emailvalidation.abstractapi.com/v1",
   params: {
-    api_key: KEY,
+    api_key: import.meta.env.VITE_EMAILVALIDATION_API_KEY,
   },
 });
